2213a05292b56c1754dd4a2f0e6f8ed9f445e6be
kwojcicki <kwojcicki@sympatico.ca>
2018-08-29 22:33:52 -0400

    group anagram

--- leetcode/src/solutions/group_anagrams.java
+++ leetcode/src/solutions/group_anagrams.java
@@ -1,5 +1,25 @@
 package solutions;
 
-public class group_anagrams {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class group_anagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+    	Map<String, List<String>> toReturn = new HashMap<String, List<String>>();
+    	for(String s: strs) {
+    		char[] c = s.toCharArray();
+    		Arrays.sort(c);
+    		
+    		if(!toReturn.containsKey(String.valueOf(c))) {
+    			toReturn.put(String.valueOf(c), new ArrayList<String>());
+    		}
+    		
+    		toReturn.get(String.valueOf(c)).add(s);
+    	}
+    	
+    	return new ArrayList<List<String>>(toReturn.values());
+    }
 }
