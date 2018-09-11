719111f5ff327f9235fdc12958d23cd8553b7cbb
kwojcicki <kwojcicki@sympatico.ca>
2018-09-10 21:52:40 -0400

    n ary tree preorder traversal

---
+++ leetcode/src/solutions/n_ary_tree_preorder_traversal.java
@@ -0,0 +1,31 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class n_ary_tree_preorder_traversal {
+    public List<Integer> preorder(Node root) {
+        List<Integer> l = new ArrayList<Integer>();
+    	if(root == null) {
+    		return l;
+    	}
+        l.add(root.val);
+        for(Node c : root.children) {
+        	l.addAll(preorder(c));
+        }
+        return l;
+    }
+}
+
+//Definition for a Node.
+class Node {
+ public int val;
+ public List<Node> children;
+
+ public Node() {}
+
+ public Node(int _val,List<Node> _children) {
+     val = _val;
+     children = _children;
+ }
+}
