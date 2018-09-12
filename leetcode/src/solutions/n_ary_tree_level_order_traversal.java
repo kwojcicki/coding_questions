3ac2b50883549eef496332d1cd7430f2f9be08c5
kwojcicki <kwojcicki@sympatico.ca>
2018-09-11 22:44:01 -0400

    finished

--- leetcode/src/solutions/n_ary_tree_level_order_traversal.java
+++ leetcode/src/solutions/n_ary_tree_level_order_traversal.java
@@ -1,10 +1,32 @@
 package solutions;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class n_ary_tree_level_order_traversal {
 	public List<List<Integer>> levelOrder(Node root) {
-		List<List<Integer>> toRet = new List<List<Integer>>();
+		List<List<Integer>> toRet = new ArrayList<List<Integer>>();
+		
+        if (root == null) {
+            return toRet;
+        }
+        
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                list.add(node.val);
+                for (Node nei : node.children) {
+                    queue.offer(nei);
+                }
+            }
+            toRet.add(list);
+        }
 		
 		return toRet;
 	}
