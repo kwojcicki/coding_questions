72c32e1bec06d1f38b1a0db8f36a8e6a06ec4782
kwojcicki <kwojcicki@sympatico.ca>
2018-09-15 00:38:31 -0400

    finished distribute candies

--- leetcode/src/solutions/distribute_candies.java
+++ leetcode/src/solutions/distribute_candies.java
@@ -1,7 +1,16 @@
 package solutions;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class distribute_candies {
     public int distributeCandies(int[] candies) {
+    	
+    	Set<Integer> set = new HashSet<Integer>();
+    	for(int n: candies) {
+    		set.add(n);
+    	}
+    	
+    	return Math.min(set.size(), candies.length / 2);
     }
-
 }
