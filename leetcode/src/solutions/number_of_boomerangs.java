b95a4a8cfc0b35cdac8a4540d55d88b97d8897b2
kwojcicki <kwojcicki@sympatico.ca>
2018-09-15 23:57:55 -0400

    finished number of boomerangs

--- leetcode/src/solutions/number_of_boomerangs.java
+++ leetcode/src/solutions/number_of_boomerangs.java
@@ -13,7 +13,19 @@ public class number_of_boomerangs {
         		if(j == i) {
         			continue;
         		}
+        		
+        		
+        		int xDiff = points[i][0] - points[j][0];
+        		int yDiff = points[i][1] - points[j][1];
+        		int diff = xDiff * xDiff + yDiff * yDiff;
+        		x.put(diff, x.getOrDefault(diff, 0) + 1);
         	}
+        	
+        	for(int n: x.values()) {
+        		toRet += n * (n-1);
+        	}
+        	
+        	x.clear();
         }
     	
     	return toRet;
