8745f0dd3c8c6da1b8c8308d86582b6f5b7fe627
kwojcicki <kwojcicki@sympatico.ca>
2018-09-20 22:54:23 -0400

    progress

--- leetcode/src/solutions/rectangle_overlap.java
+++ leetcode/src/solutions/rectangle_overlap.java
@@ -2,6 +2,6 @@ package solutions;
 
 public class rectangle_overlap {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        return rec2[0] < rec1[2]
+        return rec2[0] < rec1[2] && rec2[1] < rec1[3] && rec2[0] > rec1[0] && rec2[1] > rec1[1];
     }
 }
