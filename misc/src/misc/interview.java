9faf269bcce0b9445aacc02212e0a078921372bb
kwojcicki <kwojcicki@sympatico.ca>
2018-09-24 17:25:06 -0400

    testcase

--- misc/src/misc/interview.java
+++ misc/src/misc/interview.java
@@ -16,6 +16,18 @@ public class interview {
 	 *   Create method that can deep copy structures like this
 	 */
 	public static void main(String[] args) {
+		Node n1 = new Node(1);
+		Node n2 = new Node(2);
+		Node n3 = new Node(3);
+		
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = null;
+		
+		n1.optional = n3;
+		
+		Node newList = deepCopy(n1);
+		printList(newList);
 		
 	}
 	
@@ -35,7 +47,7 @@ public class interview {
 			head = head.next;
 		}
 		
-		
+		return newHead;
 	}
 	
 	static class Node {
