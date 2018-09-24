fb14f5cbdac3876c1bf5ada9bb67cd805d9b520f
kwojcicki <kwojcicki@sympatico.ca>
2018-09-23 23:32:47 -0400

    some progress

--- misc/src/misc/Combinations.java
+++ misc/src/misc/Combinations.java
@@ -1,15 +1,33 @@
 package misc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Combinations {
 
 	public static void main(String[] args) {
-		
+		List<Integer> x = new ArrayList<Integer>();
+		x.add(1);
+		x.add(2);
+		x.add(3);
+		System.out.println(permutations(x, x.size() - 1));
 	}
 	
-	public static <T> List<List<T>> permutations(List<T> x){
-		return null;
+	public static <T> List<List<T>> permutations(List<T> x, int size){
+		List<List<T>> lists = new ArrayList<List<T>>();
+		if(size == -1) {
+			lists.add(new ArrayList<T>());
+			return lists;
+		}
+		
+		List<List<T>> otherLists = permutations(x, size - 1);
+		for(List<T> perm: otherLists) {
+			List<T> temp = new ArrayList<T>(perm);
+			temp.add(x.get(size));
+			lists.add(temp);
+		}
+		
+		return lists;
 	}
 	
 }
