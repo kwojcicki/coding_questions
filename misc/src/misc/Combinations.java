4977943b30d5b1714b19b867d079feafb154134a
kwojcicki <kwojcicki@sympatico.ca>
2018-09-23 23:21:43 -0400

    method

--- misc/src/misc/Combinations.java
+++ misc/src/misc/Combinations.java
@@ -1,9 +1,15 @@
 package misc;
 
+import java.util.List;
+
 public class Combinations {
 
 	public static void main(String[] args) {
 		
+	}
+	
+	public static <T> List<List<T>> permutations(List<T> x){
+		return null;
 	}
 	
 }
