a3f5654b38ea60f2eb539fa90c341627081585b9
kwojcicki <kwojcicki@sympatico.ca>
2019-01-26 14:02:32 -0500

    count primes + valid sudoku

--- leetcode/src/top_interview_easy/array/valid_sudoku.java
+++ leetcode/src/top_interview_easy/array/valid_sudoku.java
@@ -1,6 +1,5 @@
 package top_interview_easy.array;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
---
+++ leetcode/src/top_interview_easy/math/count_primes.java
@@ -0,0 +1,25 @@
+package top_interview_easy.math;
+
+public class count_primes {
+	
+    public int countPrimes(int n) {
+        boolean[] notPrime = new boolean[n];
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (notPrime[i] == false) {
+                count++;
+                for (int j = 2; i*j < n; j++) {
+                    notPrime[i*j] = true;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    public static void main(String[] args) {
+    	count_primes s = new count_primes();
+    	
+    	System.out.println(s.countPrimes(499979));
+    }
+}
