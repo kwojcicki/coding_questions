6218b1e88ff030a2a85f8bf24fd725e9bcfd29b0
kwojcicki <kwojcicki@sympatico.ca>
2019-01-26 13:17:20 -0500

    valid sudoku

---
+++ leetcode/src/top_interview_easy/array/valid_sudoku.java
@@ -0,0 +1,25 @@
+package top_interview_easy.array;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class valid_sudoku {
+	
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> set = new HashSet<String>();
+        
+        for(int i = 0; i < board.length; i++) {
+        	for(int j = 0; j < board[i].length; j++) {
+        		if(board[i][j] == '.') continue;
+        		if(!set.add("column(" + board[i][j] + ")" + i) || 
+        				!set.add("row(" + board[i][j] + ")" + j) || 
+        				!set.add("corner(" + board[i][j] + ")" + i/3 + "-" + j/3)) {
+        			return false;
+        		}
+        	}
+        }
+        
+        return true;
+    }
+}
