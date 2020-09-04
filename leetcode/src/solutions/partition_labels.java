6dc552d96045d798c47e35ba59b961660b82dcbb
kwojcicki <kwojcicki@sympatico.ca>
2020-09-04 11:07:18 -0400

    partition labels

---
+++ leetcode/src/solutions/partition_labels.java
@@ -0,0 +1,52 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class partition_labels {
+    public static List<Integer> partitionLabels(String S) {
+        Map<Character, Integer[]> map = new HashMap<>();
+        List<Integer> ret = new ArrayList<>();
+        
+        for(int i = 0; i < 26; i++) {
+        	map.put((char) ('a' + i), new Integer[] { -1, -1});
+        }
+        
+        for(int i = 0; i < S.length(); i++) {
+        	if(map.get(S.charAt(i))[0] == -1) {
+        		map.get(S.charAt(i))[0] = i;
+        	}
+    		map.get(S.charAt(i))[1] = i;
+        }
+        
+        int curr = 0;
+        int sum = 0;        
+        
+        for(int i = 0; i < S.length(); i++) {
+        	System.out.println(S.charAt(i) + " " + map.get(S.charAt(i))[1] + " " + curr);
+        	sum++;
+        	
+        	if(map.get(S.charAt(i))[0] == map.get(S.charAt(i))[1] && i > curr) {
+            	ret.add(1);
+            	sum = 0;
+            	System.out.println("2");
+            } else if(map.get(S.charAt(i))[1] != curr || i != curr) {
+        		curr = Math.max(map.get(S.charAt(i))[1], curr);
+        		System.out.println("1");
+        	} else {
+        		ret.add(sum);
+        		sum = 0;
+        	}
+        }
+        
+        return ret;
+    }
+    
+    public static void main(String[] args) {
+    	System.out.println(partitionLabels("ababcbacadefegdehijhklij"));
+    	System.out.println(partitionLabels("eaaaabaaec"));
+    	
+    }
+}
