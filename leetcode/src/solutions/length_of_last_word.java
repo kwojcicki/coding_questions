356f14d9aa113bf29abbf8265287287254345cbb
kwojcicki <kwojcicki@sympatico.ca>
2020-09-15 10:23:51 -0400

    length of last word

--- leetcode/src/solutions/length_of_last_word.java
+++ leetcode/src/solutions/length_of_last_word.java
@@ -3,6 +3,7 @@ package solutions;
 public class length_of_last_word {
     public int lengthOfLastWord(String s) {
     	if(s == null || s.length() == 0) return 0;
-        return s.length() - 1 - (s.lastIndexOf(' ') > -1 ? s.lastIndexOf(' ') : 0 ); 
+    	s = s.trim();
+        return s.length() - 1 - (s.lastIndexOf(' ') > -1 ? s.lastIndexOf(' ') : -1 ); 
     }
 }
