05875c9ae985f1e2f36d29dffac7a204a94f0a7a
kwojcicki <kwojcicki@sympatico.ca>
2020-09-20 10:39:55 -0400

    subarray sum equals k

--- leetcode/src/solutions/subarray_sum_equals_k.java
+++ leetcode/src/solutions/subarray_sum_equals_k.java
@@ -1,6 +1,43 @@
 package solutions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class subarray_sum_equals_k {
+	
+	public int subarraySum2(int[] nums, int k) {
+		int count = 0;
+		int sum = 0;
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		map.put(0, 1);
+		
+		for(int i = 0; i < nums.length; i++) {
+			sum += nums[i];
+			count += map.getOrDefault(sum - k, 0);
+			map.put(sum, map.getOrDefault(sum, 0) + 1);
+		}
+		
+		return count;
+	}
+	
+	public int subarraySum1(int[] nums, int k) {
+		int[] sums = new int[nums.length + 1];
+		
+		for(int i = 0; i < nums.length; i++) {
+			sums[i + 1] = sums[i + 1] + nums[i];
+		}
+		
+		int count = 0;
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = i + 1; j < nums.length; j++) {
+				if(sums[j] - sums[i] == k) count++; 
+			}
+		}
+		
+		return count;
+	}
+	
     public int subarraySum(int[] nums, int k) {
         
     	int[] sums = new int[nums.length];
