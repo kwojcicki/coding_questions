2ce226d92e190012bcb6d77f6f8c4afb5dbc69a1
Krystian Wojcicki <kwojcicki@sympatico.ca>
2020-09-26 19:41:09 -0400

    Running sum of 1d array

---
+++ leetcode/src/solutions/running_sum_of_1d_array.java
@@ -0,0 +1,16 @@
+package solutions;
+
+import java.util.stream.IntStream;
+
+public class running_sum_of_1d_array {
+    public int[] runningSum(int[] nums) {
+        int[] ret = new int[nums.length];
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            ret[i] = sum;
+        }
+        
+        return ret;
+    }
+}
