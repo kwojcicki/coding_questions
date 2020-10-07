6e37acb03f48916805ee471c9751519e05bd3afb
kwojcicki <kwojcicki@sympatico.ca>
2020-10-07 10:28:42 -0400

    word search

---
+++ leetcode/src/solutions/word_search.java
@@ -0,0 +1,43 @@
+package solutions;
+
+public class word_search {
+    public boolean exist(char[][] board, String word) {
+		if(word == null || word.equals("")) return true;
+
+		boolean found = false;
+
+		for(int r = 0; r < board.length; r++) {
+			for(int c = 0; c < board[r].length; c++) {
+				if(board[r][c] == word.charAt(0)) {
+                    //System.out.println(r + " " + c);
+					boolean[][] seen = new boolean[board.length][board[r].length];
+					found = found || dfs(board, r, c, seen, word, 0);
+                    if(found) return found;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	public boolean dfs(char[][] board, int r, int c, boolean[][] seen, String word, int index) {
+        
+        if(index == word.length()) return true;
+
+		if(r < 0 || c < 0 || r >= seen.length || c >= seen[r].length || seen[r][c]) return false;
+        
+        
+		if(board[r][c] != word.charAt(index)) return false;
+
+		seen[r][c] = true;
+
+		boolean result =  dfs(board, r + 1, c, seen, word, index + 1) ||
+				dfs(board, r - 1, c, seen, word, index + 1) ||
+				dfs(board, r, c + 1, seen, word, index + 1) ||
+				dfs(board, r, c - 1, seen, word, index + 1);
+		
+		seen[r][c] = false;
+		
+		return result;
+	}
+}
