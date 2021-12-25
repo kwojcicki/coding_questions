176c097e1c2d7b83a2cace9b2991a305365d4a2a
kwojcicki <kwojcicki@sympatico.ca>
2021-12-25 13:49:02 -0500

    basic calculator ii

--- leetcode/src/solutions/basic_calculator_ii.java
+++ leetcode/src/solutions/basic_calculator_ii.java
@@ -3,7 +3,7 @@ package solutions;
 import java.util.Stack;
 
 public class basic_calculator_ii {
-    public int calculate(String s) {
+    public int calculate1(String s) {
         if(s == null || s.length() == 0) {
         	return 0;
         }
@@ -48,5 +48,99 @@ public class basic_calculator_ii {
         }
         
         return ret;
+    }
+    
+
+    public int calculateDiscussion(String s) {
+    	if(s == null || s.length() == 0) return 0;
+    	s = s.trim();
+    	char oper = '+';
+    	int number = 0;
+    	char[] c = s.toCharArray();
+    	Stack<Integer> n = new Stack<>();
+    	
+    	for(int i = 0; i < c.length; i++) {
+
+    		if(Character.isDigit(c[i])) {
+    			number = number * 10 + (c[i] - '0');
+    		}
+            
+            if(c[i] == ' ' || (Character.isDigit(c[i]) && i != c.length - 1)) {
+    			continue;
+    		}
+    		
+    		if(oper == '+') {
+    			n.push(number);
+    		} else if(oper == '-') {
+    			n.push(- number);
+    		} else if(oper == '*') {
+    			n.push(n.pop() * number);
+    		} else if(oper == '/') {
+    			n.push(n.pop() / number);
+    		}
+    		oper = c[i];
+    		number = 0;
+    	}
+    	
+    	return n.stream().reduce((i, j) -> i + j).orElse(0);
+    }
+    
+    public int calculate(String s) {
+    	int ret = 0;
+    	
+    	if(s == null || s.length() == 0) {
+    		return 0;
+    	}
+    	
+    	Stack<Integer> nums = new Stack<>();
+    	Stack<Character> opers = new Stack<>();
+    	
+    	char[] c = s.toCharArray();
+    	int i = 0;
+    	
+    	while(i < c.length) {
+    		if(c[i] == ' ') {
+    			i++;
+    			continue;
+    		} else if(Character.isDigit(c[i])) {
+    			int number = 0;
+    			while(i < c.length && Character.isDigit(c[i]) ) {
+    				number = number * 10 + (c[i] - '0');
+        			i++;
+    			}
+    			nums.push(number);
+    			
+    			if(!opers.isEmpty() && opers.peek() == '*') {
+    				opers.pop();
+    				int num1 = nums.pop();
+    				int num2 = nums.pop();
+    				nums.push(num1 * num2);
+    			} else if(!opers.isEmpty() && opers.peek() == '/') {
+    				opers.pop();
+    				int num1 = nums.pop();
+    				int num2 = nums.pop();
+    				nums.push(num2 / num1);
+    			}
+    		} else {
+    			opers.push(c[i]);
+    			i++;
+    		}
+    	}
+    	
+    	if( nums.size() == 1) return nums.peek();
+    	
+    	
+    	while(!nums.isEmpty()) {
+    		int num1 = nums.pop();
+    		char oper = opers.isEmpty() ? '+' : opers.pop();
+    		
+    		if(oper == '+') {
+    			ret += num1;
+    		} else if(oper == '-') {
+    			ret -= num1;
+    		}
+    	}
+    	
+    	return ret;
     }
 }
