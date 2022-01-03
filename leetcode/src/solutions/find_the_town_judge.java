dbf24f00a8d091e44619126ccf5617fcd6a42d4b
kwojcicki <kwojcicki@sympatico.ca>
2022-01-03 12:02:39 -0500

    find the two judge

---
+++ leetcode/src/solutions/find_the_town_judge.java
@@ -0,0 +1,20 @@
+package solutions;
+
+public class find_the_town_judge {
+    public int findJudge(int n, int[][] trust) {
+        int[] trusts = new int[n];
+        
+        for(int[] t: trust){
+            trusts[t[0] - 1]--;
+            trusts[t[1] - 1]++;
+        }
+        
+        for(int i = 0; i < n; i++) {
+        	if(trusts[i] == n - 1) {
+        		return i + 1;
+        	}
+        }
+        
+        return -1;
+    }
+}
