2330bbb8e09327d849559104c1d573948683e0af
kwojcicki <kwojcicki@sympatico.ca>
2022-01-03 12:06:01 -0500

    find the town judge

--- leetcode/src/solutions/find_the_town_judge.java
+++ leetcode/src/solutions/find_the_town_judge.java
@@ -1,22 +1,22 @@
 package solutions;
 
 public class find_the_town_judge {
-    public int findJudge(int n, int[][] trust) {
-        int[] trusts = new int[n];
-        
-        if(trust.length < n - 1) return -1;
-        
-        for(int[] t: trust){
-            trusts[t[0] - 1]--;
-            trusts[t[1] - 1]++;
-        }
-        
-        for(int i = 0; i < n; i++) {
-        	if(trusts[i] == n - 1) {
-        		return i + 1;
-        	}
-        }
-        
-        return -1;
-    }
+	public int findJudge(int n, int[][] trust) {
+		int[] trusts = new int[n];
+
+		if(trust.length < n - 1) return -1;
+
+		for(int[] t: trust){
+			trusts[t[0] - 1]--;
+			trusts[t[1] - 1]++;
+		}
+
+		for(int i = 0; i < n; i++) {
+			if(trusts[i] == n - 1) {
+				return i + 1;
+			}
+		}
+
+		return -1;
+	}
 }
