9d953f0096eb0102fa3c321c0a96368d4f0e9ae3
kwojcicki <kwojcicki@sympatico.ca>
2022-01-04 11:28:33 -0500

    complement of base 10 integer

--- leetcode/src/solutions/complement_of_base_10_integer.java
+++ leetcode/src/solutions/complement_of_base_10_integer.java
@@ -1,7 +1,18 @@
 package solutions;
 
 public class complement_of_base_10_integer {
-    public int bitwiseComplement(int N) {
+    public int bitwiseComplement1(int N) {
     	return ((int)Math.pow(2, Integer.toBinaryString(N).length()) - 1) ^ N;
+    }
+    
+    public int bitwiseComplement(int N) {
+        int mask = 1;
+    	while(mask < N) {
+    		mask = (mask << 1) + 1;
+    	}
+    	// 1111111
+    	// 1010101
+    	
+    	return ~N & mask;
     }
 }
