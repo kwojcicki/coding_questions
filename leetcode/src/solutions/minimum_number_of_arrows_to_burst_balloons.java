c150dcb68f71cad58045b0884dcf088aee68c22b
kwojcicki <kwojcicki@sympatico.ca>
2022-01-13 19:36:47 -0500

    minimum number of arrows to burst balloons

--- leetcode/src/solutions/minimum_number_of_arrows_to_burst_balloons.java
+++ leetcode/src/solutions/minimum_number_of_arrows_to_burst_balloons.java
@@ -3,11 +3,47 @@ package solutions;
 import java.util.Arrays;
 
 public class minimum_number_of_arrows_to_burst_balloons {
+	
 	public int findMinArrowShots(int[][] points) {
+		Arrays.sort(points, (a,b) -> Integer.compare(a[1], b[1]));
+		int ret = 0, min = points[0][1];
+		
+		for(int i = 0; i < points.length; i++) {
+			if(points[i][0] > min) {
+				ret++;
+				min = points[i][1];
+			}
+		}
+		
+		return ret + 1;
+	}
+	
+    public int findMinArrowShots2(int[][] points) {
+    	Arrays.sort(points, (a,b) -> Integer.compare(a[0], b[0]));
+    	
+    	if(points == null || points.length == 0) {
+    		return 0;
+    	}
+    	
+    	int ret = 0;
+		int min = points[0][1];
+		
+    	for(int i = 0; i < points.length; i++) {
+    		if(points[i][0] > min) {
+    			ret++;
+    			min = points[i][1];
+    		} else if(points[i][1] < min) {
+    			min = points[i][1];
+    		}
+    	}
+    	
+    	return ret + 1;
+    }
+	
+	public int findMinArrowShots1(int[][] points) {
 		Arrays.sort(points, (a,b)->{
 			return Integer.compare(a[1], b[1]);
 		});
-
 
 		//         for(int[] a: points){
 		//             System.out.println(Arrays.toString(a));
@@ -23,9 +59,6 @@ public class minimum_number_of_arrows_to_burst_balloons {
 				max = points[i][1];
 			}
 		}
-
-
-
 		return points.length - overlapping;
 	}
 }
