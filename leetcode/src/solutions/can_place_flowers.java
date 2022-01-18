5e16363be9004205aa5571d1c1a7b3ee2d31cf5a
kwojcicki <kwojcicki@sympatico.ca>
2022-01-18 18:34:57 -0500

    can place flowers

--- leetcode/src/solutions/can_place_flowers.java
+++ leetcode/src/solutions/can_place_flowers.java
@@ -1,7 +1,28 @@
 package solutions;
 
+import java.util.Arrays;
+
 public class can_place_flowers {
+	
 	public boolean canPlaceFlowers(int[] flowerbed, int n) {
+		
+		int ret = 0;
+		int prev = 0;
+		int next = flowerbed.length > 1 ? flowerbed[1] : 0; 
+
+		for(int i = 0; i < flowerbed.length; i++) {
+			if(prev == 0 && next == 0 && flowerbed[i] == 0) {
+				ret++;
+				flowerbed[i] = 1;
+			}
+			prev = flowerbed[i];
+			next = (i + 2) <= flowerbed.length - 1 ? flowerbed[i + 2] : 0;
+		}
+
+		return ret >= n;
+	}
+	
+	public boolean canPlaceFlowers1(int[] flowerbed, int n) {
 
 		if(flowerbed == null || flowerbed.length == 0) {
 			return n == 0;
