cd33d0e7775620e08f683693e711fbe2c2354a6e
kwojcicki <kwojcicki@sympatico.ca>
2022-02-02 18:38:02 -0500

    find all anagrams in a string

--- leetcode/src/solutions/find_all_anagrams_in_a_string.java
+++ leetcode/src/solutions/find_all_anagrams_in_a_string.java
@@ -1,10 +1,39 @@
 package solutions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class find_all_anagrams_in_a_string {
 	public List<Integer> findAnagrams(String s, String p) {
+		List<Integer> ret = new ArrayList<>();
+
+		char[] curr = new char[26];
+		char[] past = new char[26];
+
+		for(char c: p.toCharArray()) {
+			past[c - 'a']++;
+		}
+
+		for(int i = 0; i < s.length() && i < p.length(); i++) {
+			curr[s.charAt(i) - 'a']++;
+		}
+
+		for(int i = p.length(); i < s.length(); i++) {
+			if(Arrays.equals(curr, past)) {
+				ret.add(i - p.length());
+			}
+
+			curr[s.charAt(i - p.length()) - 'a']--;
+			curr[s.charAt(i) - 'a']++;
+		}
+		if(Arrays.equals(curr, past)) {
+			ret.add(s.length() - p.length());
+		}
+		return ret;
+	}
+	
+	public List<Integer> findAnagrams1(String s, String p) {
 		List<Integer> ret = new ArrayList<>();
 
 		char[] curr = new char[26];
