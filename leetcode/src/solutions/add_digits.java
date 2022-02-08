b1bcb6bcec5eadc5e2b46b868e817a6daaf16052
kwojcicki <kwojcicki@sympatico.ca>
2022-02-08 11:34:07 -0500

    add digit

--- leetcode/src/solutions/add_digits.java
+++ leetcode/src/solutions/add_digits.java
@@ -1,7 +1,14 @@
 package solutions;
 
 public class add_digits {
+	
     public int addDigits(int num) {
+        if(num < 10) return num;
+        if(num % 9 == 0) return 9;
+    	return num % 9;
+    }
+	
+    public int addDigits1(int num) {
         while(num >= 10) {
     		int tmp = 0;
     		while(num >= 10) {
