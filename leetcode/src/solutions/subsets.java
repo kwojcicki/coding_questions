9529b1a6d5591eb89cec2cb27f6eb4a29924fc58
kwojcicki <kwojcicki@sympatico.ca>
2022-02-13 12:35:14 -0500

    subsets

--- leetcode/src/solutions/subsets.java
+++ leetcode/src/solutions/subsets.java
@@ -4,7 +4,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class subsets {
+	
     public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<>();
+        recurse(nums, 0, ret, new ArrayList<>());
+        return ret;
+    }
+	
+	public void recurse(int[] nums, int index, List<List<Integer>> ret, List<Integer> list) {
+		if(index == nums.length) {
+			ret.add(new ArrayList<>(list));
+			return;
+		}
+		
+		list.add(nums[index]);
+		recurse(nums, index + 1, ret, list);
+		list.remove(list.size() - 1);
+		recurse(nums, index + 1, ret, list);
+	}
+	
+    public List<List<Integer>> subsets1(int[] nums) {
         List<List<Integer>> ret = new ArrayList<>();
         
         helper(ret, new ArrayList<>(), nums, 0);
