59be2bcb618cbff682b81bc1bc53e5f6e244da34
kwojcicki <kwojcicki@sympatico.ca>
2022-02-27 23:10:23 -0500

    summary ranges

--- leetcode/src/solutions/summary_ranges.java
+++ leetcode/src/solutions/summary_ranges.java
@@ -4,7 +4,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class summary_ranges {
+
 	public List<String> summaryRanges(int[] nums) {
+		List<String> ret = new ArrayList<>();
+
+		if(nums == null || nums.length == 0) {
+			return ret;
+		}
+
+		int previous = nums[0];
+		for(int i = 1; i < nums.length; i++) {
+			if(nums[i] != nums[i - 1] + 1) {
+				if(previous == nums[i - 1]) {
+					ret.add(previous + "");
+				} else {
+					ret.add(previous +"->" + nums[i - 1]);
+				}
+				previous = nums[i];
+			}
+		}
+
+		if(nums[nums.length - 1] != previous) {
+			ret.add(previous +"->" + nums[nums.length - 1]);
+		} else {
+			ret.add(previous + "");
+		}
+
+		return ret;
+	}
+	
+	public List<String> summaryRanges1(int[] nums) {
 		List<String> ret = new ArrayList<>();
 		if(nums == null || nums.length == 0) return ret;
 
