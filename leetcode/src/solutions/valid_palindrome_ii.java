92d6af1eb834864a5ff85a3e572a40b0804f71b8
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-04-01 19:28:43 -0700

    valid palindrome

--- leetcode/src/solutions/valid_palindrome_ii.java
+++ leetcode/src/solutions/valid_palindrome_ii.java
@@ -1,7 +1,32 @@
 package solutions;
 
 public class valid_palindrome_ii {
-	public boolean validPalindrome(String s) {
+    public boolean validPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        
+        while(start < end) {
+        	if(s.charAt(start) != s.charAt(end)) {
+        		return isPalindrome(start + 1, end, s) || isPalindrome(start, end - 1, s);
+        	}
+        	start++;
+        	end--;
+        }
+        
+        return true;
+    }
+    
+    public boolean isPalindrome(int start, int end, String s) {
+    	while(start < end) {
+    		if(s.charAt(start) != s.charAt(end)) return false;
+    		start++;
+    		end--;
+    	}
+    	
+    	return true;
+    }
+    
+	public boolean validPalindrome1(String s) {
 
 		int start = 0, end = s.length() - 1;
 
