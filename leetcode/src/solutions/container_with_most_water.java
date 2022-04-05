23e8ba13295c5aaca22c00f905f59f5464046bbd
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-04-04 18:07:47 -0700

    container with most water

--- leetcode/src/solutions/container_with_most_water.java
+++ leetcode/src/solutions/container_with_most_water.java
@@ -1,7 +1,24 @@
 package solutions;
 
 public class container_with_most_water {
-	public int maxArea(int[] height) {
+	
+    public int maxArea(int[] height) {
+        int start = 0;
+        int end = height.length - 1;
+        int max = 0;
+        while(start < end) {
+        	max = Math.max(Math.min(height[start], height[end]) * (end - start), max);
+        	if(height[start] > height[end]) {
+        		end--;
+        	} else {
+        		start++;
+        	}
+        }
+        
+        return max;
+    }
+	
+	public int maxArea1(int[] height) {
 		int start = 0;
 		int end = height.length - 1;
 		int max = 0;
