2e3caf213f1ac7e364ff77b241a7b755b684dfc9
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-04-15 15:24:17 -0700

    diameter of a binary tree

--- leetcode/src/solutions/diameter_of_binary_tree.java
+++ leetcode/src/solutions/diameter_of_binary_tree.java
@@ -1,7 +1,23 @@
 package solutions;
 
 public class diameter_of_binary_tree {
+	int max = 0;
     public int diameterOfBinaryTree(TreeNode root) {
+    	helper(root);
+        return max;
+    }
+    
+    public int helper(TreeNode root) {
+    	if(root == null) return 0;
+    	
+    	int left = helper(root.left);
+    	int right = helper(root.right);
+    	
+    	max = Math.max(max, left + right);
+    	return 1 + Math.max(left, right);
+    }
+    
+    public int diameterOfBinaryTree1(TreeNode root) {
         return recurse(root)[0];
     }
     
