ad8b32d0082203f6bd344828b4ab96d6494ad0e7
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-04-16 11:59:21 -0700

    product of array except self

--- leetcode/src/solutions/product_of_array_except_self.java
+++ leetcode/src/solutions/product_of_array_except_self.java
@@ -1,23 +1,47 @@
 package solutions;
 
+import java.util.Arrays;
+
 public class product_of_array_except_self {
-    public int[] productExceptSelf(int[] nums) {
-        int[] ret = new int[nums.length];
-        
-        int left = nums[0];
-        ret[0] = 1;
-        
-        for(int i = 1; i < nums.length; i++) {
-        	ret[i] = left;
-        	left *= nums[i];
-        }
-        
-        int right = nums[nums.length - 1];
-        for(int i = nums.length - 2; i >= 0; i--) {
-        	ret[i] *= right;
-        	right *= nums[i];
-        }
-        
-        return ret;
-    }
+
+
+	public int[] productExceptSelf(int[] nums) {
+		int[] ret = new int[nums.length];
+		Arrays.fill(ret, 1);
+
+		int accum = 1;
+
+		for(int i = 0; i < ret.length; i++) {
+			ret[i] = accum;
+			accum *= nums[i];
+		}
+
+		accum = 1;
+		for(int i = ret.length - 1; i >= 0; i--) {
+			ret[i] *= accum;
+			accum *= nums[i];
+		}
+
+		return ret;
+	}
+
+	public int[] productExceptSelf1(int[] nums) {
+		int[] ret = new int[nums.length];
+
+		int left = nums[0];
+		ret[0] = 1;
+
+		for(int i = 1; i < nums.length; i++) {
+			ret[i] = left;
+			left *= nums[i];
+		}
+
+		int right = nums[nums.length - 1];
+		for(int i = nums.length - 2; i >= 0; i--) {
+			ret[i] *= right;
+			right *= nums[i];
+		}
+
+		return ret;
+	}
 }
