1c45a5de4a8337256828240a120cd62c73f59b0a
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-04-15 19:17:56 -0700

    convert bst to greater tree

--- leetcode/src/solutions/convert_bst_to_greater_tree.java
+++ leetcode/src/solutions/convert_bst_to_greater_tree.java
@@ -2,8 +2,25 @@ package solutions;
 
 public class convert_bst_to_greater_tree {
 	
-	static int sum = 0;
+    int curr = 0;
     public TreeNode convertBST(TreeNode root) {
+        helper(root);
+        return root;
+    }
+    
+    public void helper(TreeNode root){
+        if(root == null) return;
+        
+        helper(root.right);
+        
+        curr += root.val;
+        root.val = curr;
+        
+        helper(root.left);
+    }
+	
+	static int sum = 0;
+    public TreeNode convertBST1(TreeNode root) {
     	sum = 0;
     	recurse(root);
     	return root;
