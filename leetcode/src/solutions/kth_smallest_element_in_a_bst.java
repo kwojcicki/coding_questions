22fcbfa618fb4adeb5d26d0a96c2be7e1c032a0b
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-04-17 18:26:58 -0700

    kth mallest element in a bst

--- leetcode/src/solutions/kth_smallest_element_in_a_bst.java
+++ leetcode/src/solutions/kth_smallest_element_in_a_bst.java
@@ -2,10 +2,31 @@ package solutions;
 
 public class kth_smallest_element_in_a_bst {
 	
+    private int k;
+    public int kthSmallest(TreeNode root, int k) {
+        this.k = k;
+        return helper(root);
+    }
+    
+    private int helper(TreeNode root){
+        if(root == null) return -1;
+        
+        int right = helper(root.left);
+        if(right != -1) return right;
+        
+        if(this.k == 1) return root.val;
+        k--;
+        
+        int left = helper(root.right);
+        if(left != -1) return left;
+        
+        return -1;
+    }
+	
 	private static int count = 0;
 	private static int number = 0;
 	
-    public int kthSmallest(TreeNode root, int k) {
+    public int kthSmallest1(TreeNode root, int k) {
         kthSmallestHelper(root, k);
         return number;
     }
