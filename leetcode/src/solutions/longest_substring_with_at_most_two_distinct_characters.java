b67ec47ab2d8a147ff5cd394f87c12b2cb1864c1
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-04-21 12:28:15 -0700

    longest substring with at most two distinct characters

--- leetcode/src/solutions/longest_substring_with_at_most_two_distinct_characters.java
+++ leetcode/src/solutions/longest_substring_with_at_most_two_distinct_characters.java
@@ -1,10 +1,43 @@
 package solutions;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class longest_substring_with_at_most_two_distinct_characters {
+	
     public int lengthOfLongestSubstringTwoDistinct(String s) {
+        if(s == null || s.length() == 0) {
+        	return 0;
+        }
+        
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int curr = 0;
+        
+        for(int i = 0; i < s.length(); i++) {
+        	map.put(s.charAt(i), i);
+        	if(map.size() == 3) {
+        		char lowest = s.charAt(i);
+        		for(Map.Entry<Character, Integer> entry : map.entrySet()) {
+        			if(entry.getValue() < map.get(lowest)) {
+        				lowest = entry.getKey();
+        			}
+        		}
+        		
+        		Integer pos = map.remove(lowest);
+        		curr = i - pos;
+        	} else {
+        		curr++;
+        	}
+        	max = Math.max(max, curr);
+        }
+        
+        return max;
+    }
+	
+    public int lengthOfLongestSubstringTwoDistinct1(String s) {
         if(s == null || s.length() == 0) return 0;
         
         int start = 0;
