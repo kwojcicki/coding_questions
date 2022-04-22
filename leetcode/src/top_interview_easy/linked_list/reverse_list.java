92a28ffee8646f6fde46504119d8011e29ddbd65
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-04-21 17:10:18 -0700

    reverse list

--- leetcode/src/top_interview_easy/linked_list/reverse_list.java
+++ leetcode/src/top_interview_easy/linked_list/reverse_list.java
@@ -1,8 +1,23 @@
 package top_interview_easy.linked_list;
 
 public class reverse_list {
-	
+
     public ListNode reverseList(ListNode head) {
+    	return helper(head, null);
+    }
+    
+    public ListNode helper(ListNode head, ListNode newHead) {
+    	if(head == null) {
+    		return newHead;
+    	}
+    	
+    	ListNode next = head.next;
+    	head.next = newHead;
+    	
+    	return helper(next, head);
+    }
+    
+    public ListNode reverseList1(ListNode head) {
     	if(head == null) {
     		return null;
     	}
