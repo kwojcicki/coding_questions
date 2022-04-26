dbf88f060dc9c2922330d11d9c9315b9671ec4ac
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-04-25 20:46:21 -0700

    peeking iterator

--- leetcode/src/solutions/peeking_iterator.java
+++ leetcode/src/solutions/peeking_iterator.java
@@ -4,38 +4,33 @@ import java.util.Iterator;
 
 public class peeking_iterator {
 
-class PeekingIterator implements Iterator<Integer> {
-	Iterator<Integer> iterator;
-	Integer peek = null;
-	public PeekingIterator(Iterator<Integer> iterator) {
-	    // initialize any member here.
-	    this.iterator = iterator;
-	}
-	
-    // Returns the next element in the iteration without advancing the iterator.
-	public Integer peek() {
-        if(peek != null) return peek;
-        peek = iterator.next();
-        
-        return peek;
-	}
-	
-	// hasNext() and next() should behave the same as in the Iterator interface.
-	// Override them if needed.
-	@Override
-	public Integer next() {
-		if(peek == null) {
-			return iterator.next();
+	class PeekingIterator implements Iterator<Integer> {
+		Iterator<Integer> iterator;
+		Integer peek = null;
+		public PeekingIterator(Iterator<Integer> iterator) {
+			// initialize any member here.
+			this.iterator = iterator;
+			if(this.iterator.hasNext())
+				peek = iterator.next();
 		}
-		
-		Integer tmp = peek;
-		peek = null;
-		return tmp;
+
+		// Returns the next element in the iteration without advancing the iterator.
+		public Integer peek() {
+			return peek;
+		}
+
+		// hasNext() and next() should behave the same as in the Iterator interface.
+		// Override them if needed.
+		@Override
+		public Integer next() {
+			Integer tmp = peek;
+			peek = iterator.hasNext() ? iterator.next() : null;
+			return tmp;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return peek != null;
+		}
 	}
-	
-	@Override
-	public boolean hasNext() {
-	    return peek != null || iterator.hasNext();
-	}
-}
 }
