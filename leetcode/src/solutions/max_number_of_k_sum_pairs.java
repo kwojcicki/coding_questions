f1f0f8f0f79b4b692fa4b3acc140e03b00ddb619
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-05-03 18:45:00 -0700

    max number of k sum pairs

--- leetcode/src/solutions/max_number_of_k_sum_pairs.java
+++ leetcode/src/solutions/max_number_of_k_sum_pairs.java
@@ -4,19 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class max_number_of_k_sum_pairs {
-    public int maxOperations(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int ret = 0;
-        
-        for(int i: nums) {
-        	if(map.getOrDefault(k - i, 0) > 0) {
-        		map.compute(k - i, (ky, v) -> v - 1);
-        		ret++;
-        	} else {
-        		map.put(i, map.getOrDefault(i, 0) + 1);
-        	}
-        }
-        
-        return ret;
-    }
+	public int maxOperations(int[] nums, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+		int ret = 0;
+
+		for(int i: nums) {
+			if(map.getOrDefault(k - i, 0) > 0) {
+				map.compute(k - i, (ky, v) -> v - 1);
+				ret++;
+			} else {
+				map.put(i, map.getOrDefault(i, 0) + 1);
+			}
+		}
+
+		return ret;
+	}
 }
