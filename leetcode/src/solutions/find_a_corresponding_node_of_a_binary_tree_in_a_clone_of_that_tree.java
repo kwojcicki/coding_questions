7f3b42931affda5c72cb1608c2dfbc6553248dd8
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-05-16 17:07:47 -0700

    find a corresponding node of a binray tree in a clone of that tree

---
+++ leetcode/src/solutions/find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.java
@@ -0,0 +1,14 @@
+package solutions;
+
+public class find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree {
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if(original == target) return cloned;
+        
+        if(original == null) return null;
+        
+        TreeNode left = getTargetCopy(original.left, cloned.left, target);
+        TreeNode right = getTargetCopy(original.right, cloned.right, target);
+        
+        return left == null ? right : left;
+    }
+}
