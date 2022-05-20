36ead727e231bc34dcac24d4ee08331589a13895
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-05-19 17:16:20 -0700

    unqiue paths ii

--- leetcode/src/solutions/unique_paths_ii.java
+++ leetcode/src/solutions/unique_paths_ii.java
@@ -2,20 +2,25 @@ package solutions;
 
 public class unique_paths_ii {
 
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        if(obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1] == 1) return 0;
-        
-        int[][] dp = new int[obstacleGrid.length + 1][obstacleGrid[0].length + 1];
-        
-        dp[obstacleGrid.length - 1][obstacleGrid[0].length - 1] = 1;
-        
-        for(int i = obstacleGrid.length - 1; i >= 0; i--) {
-        	for(int j = obstacleGrid[i].length - 1; j >= 0; j--) {
-        		if(dp[i][j] != 0 || obstacleGrid[i][j] == 1) continue;
-        		dp[i][j] = dp[i + 1][j] + dp[i][j + 1];
-        	}
-        }
-        
-        return dp[0][0]; 
-    }
+	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+		if(obstacleGrid[obstacleGrid.length - 1][obstacleGrid[0].length - 1] == 1) return 0;
+
+		int[] dp = new int[obstacleGrid[0].length + 1];
+
+		dp[obstacleGrid[0].length - 1] = 1;
+
+		for(int i = obstacleGrid.length - 1; i >= 0; i--) {
+			for(int j = obstacleGrid[i].length - 1; j >= 0; j--) {
+				if( (i == obstacleGrid.length - 1 && j == obstacleGrid[i].length - 1)) continue;
+				if(obstacleGrid[i][j] == 1 ){
+					dp[j] = 0;
+				} else {
+					dp[j] += dp[j + 1];
+				}
+			}
+			// System.out.println(Arrays.toString(dp));
+		}
+
+		return dp[0]; 
+	}
 }
