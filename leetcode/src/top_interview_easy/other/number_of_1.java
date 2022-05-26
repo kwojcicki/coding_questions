43a5110a25e3a2a54b44634b28377f37b98e5077
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-05-25 18:35:53 -0700

    number of 1

--- leetcode/src/top_interview_easy/other/number_of_1.java
+++ leetcode/src/top_interview_easy/other/number_of_1.java
@@ -3,6 +3,18 @@ package top_interview_easy.other;
 public class number_of_1 {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
+        int ret = 0;
+        
+        for(int i = 0; i < 32; i++){
+            ret += n & 1;
+            n >>= 1;
+        }
+        
+        return ret;
+    }
+	
+    // you need to treat n as an unsigned value
+    public int hammingWeight1(int n) {
         return Integer.bitCount(n);
     }
 }
