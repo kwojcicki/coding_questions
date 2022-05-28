adb86c08e9fa92a4122aef7694d3f88731abc8fd
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-05-27 17:12:31 -0700

    missing number

--- leetcode/src/top_interview_easy/other/missing_number.java
+++ leetcode/src/top_interview_easy/other/missing_number.java
@@ -2,6 +2,15 @@ package top_interview_easy.other;
 
 public class missing_number {
     public int missingNumber(int[] nums) {
+        int ret = (nums.length) * (nums.length + 1) / 2;
+    	for(int i: nums) {
+    		ret -= i;
+    	}
+    	
+    	return ret;
+    }
+    
+    public int missingNumber1(int[] nums) {
         int toGet = 0;
         for(int i = 0; i < nums.length + 1; i++) {
         	toGet += i;
