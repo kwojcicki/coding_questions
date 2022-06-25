a85c016cecd3b84dfff99d4507c89d59bb7da87b
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-06-25 15:12:53 -0700

    non decreasing array

--- leetcode/src/solutions/non_decreasing_array.java
+++ leetcode/src/solutions/non_decreasing_array.java
@@ -1,15 +1,18 @@
 package solutions;
 
 public class non_decreasing_array {
-    public boolean checkPossibility(int[] nums) {
-        boolean switched = false;
-        
-        for(int i = 1; i < nums.length; i++) {
-        	if(nums[i] > nums[i - 1]) continue;
-        	else if(switched) return false;
-        	switched = true;
-        }
-        
-        // return true;
-    }
+	public boolean checkPossibility(int[] nums) {
+		boolean switched = false;
+
+		for(int i = 1; i < nums.length; i++) {
+			if(nums[i] >= nums[i - 1]) continue;
+			if(switched) return false;
+			switched = true;
+
+			if(i - 2 < 0 || nums[i - 2] <= nums[i]) nums[i - 1] = nums[i];
+			else nums[i] = nums[i - 1];
+		}
+
+		return true;
+	}
 }
