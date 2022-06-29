76656f4daa54d6600b9d16bdee02036197079fd7
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-06-28 17:49:13 -0700

    queue reconstruction by height

---
+++ leetcode/src/solutions/queue_reconstruction_by_height.java
@@ -0,0 +1,29 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class queue_reconstruction_by_height {
+
+	public int[][] reconstructQueue(int[][] people) {
+		List<int[]> ret = new ArrayList<>(people.length);
+
+		Arrays.sort(people, Comparator.<int[]>comparingInt(p -> p[1]).
+				thenComparing(Comparator.comparingInt(p -> p[0])));
+
+		for(int[] p: people) {
+			int count = p[1];
+			int index = 0;
+			while(index < ret.size() && (p[0] > ret.get(index)[0] || count > 0)) {
+				if(p[0] <= ret.get(index)[0]) count--;
+				index++;
+			}
+
+			ret.add(index, p);
+		}
+
+		return ret.toArray(new int[ret.size()][2]);
+	}
+}
