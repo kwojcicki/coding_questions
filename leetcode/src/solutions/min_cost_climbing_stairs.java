a63bba082205dcea5645eaf0a9fb9ec7b4dbce75
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-07-09 17:14:42 -0700

    min cost climbing stairs

--- leetcode/src/solutions/min_cost_climbing_stairs.java
+++ leetcode/src/solutions/min_cost_climbing_stairs.java
@@ -1,7 +1,21 @@
 package solutions;
 
 public class min_cost_climbing_stairs {
+	
     public int minCostClimbingStairs(int[] cost) {
+    	int one = cost[1];
+    	int two = cost[0];
+    	int tmp = 0;
+    	for(int i = 2; i < cost.length; i++) {
+    		tmp = one;
+    		one = Math.min(two, one) + cost[i];
+    		two = tmp;
+    	}
+    	
+    	return Math.min(one, two);
+    }
+	
+    public int minCostClimbingStairs1(int[] cost) {
     	int temp = 0;
     	int oneStep = 0;
     	int twoStep = 0;
