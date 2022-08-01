a4211c6e3b85fc3718bf55b078ef54de704c33c0
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-07-31 19:23:49 -0700

    unique paths

--- leetcode/src/top_interview_medium/dp/unique_paths.java
+++ leetcode/src/top_interview_medium/dp/unique_paths.java
@@ -4,6 +4,19 @@ import java.util.Arrays;
 
 public class unique_paths {
     public int uniquePaths(int m, int n) {
+    	int[] paths = new int[n];
+    	paths[n - 1] = 1;
+    	
+    	for(int i = 0; i < m; i++) {
+    		for(int j = n - 2; j >= 0; j--) {
+    			paths[j] += paths[j + 1];
+    		}
+    	}
+    	
+    	return paths[0];
+    }
+    
+    public int uniquePaths1(int m, int n) {
     	int[][] paths = new int[m + 1][n + 1];
     	paths[m - 1][n] = 1;
         for(int i = m - 1; i >= 0; i--) {
