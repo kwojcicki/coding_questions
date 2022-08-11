4bfd3a691cb7e5a33660b69d53f37053c86b10b8
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-08-10 17:07:33 -0700

    validate binary search tree

---
+++ leetcode/src/solutions/validate_binary_search_tree.java
@@ -0,0 +1,14 @@
+package solutions;
+
+public class validate_binary_search_tree {
+	public boolean isValidBST(TreeNode root) {
+        return valid(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+    
+    public boolean valid(TreeNode root, long min, long max) {
+    	if(root == null) return true;
+    	if(root.val < min || root.val > max) return false;
+    	return valid(root.left, min, ((long)root.val) - 1) && 
+    			valid(root.right, ((long)root.val) + 1, max);
+    }
+}
