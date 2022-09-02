dc0adf0d08ba8e125233588f8729686abfef3468
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-09-01 17:48:12 -0700

    average of levels in binary tree

--- leetcode/src/solutions/average_of_levels_in_binary_tree.java
+++ leetcode/src/solutions/average_of_levels_in_binary_tree.java
@@ -1,5 +1,6 @@
 package solutions;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,6 +8,25 @@ import java.util.Queue;
 
 public class average_of_levels_in_binary_tree {
     public List<Double> averageOfLevels(TreeNode root) {
+    	List<Double> ret = new ArrayList<>();
+    	ArrayDeque<TreeNode> q = new ArrayDeque<>();
+    	q.add(root);
+    	
+    	while(!q.isEmpty()) {
+    		Double curr = 0d;
+    		int size = q.size();
+    		for(int i = 0; i < size; i++) {
+    			TreeNode n = q.poll();
+    			curr += n.val;
+    			if(n.left != null) q.add(n.left);
+    			if(n.right != null) q.add(n.right);
+    		}
+    		ret.add(curr / size);
+    	}
+    	return ret;
+    }
+	
+    public List<Double> averageOfLevels1(TreeNode root) {
         
     	List<Double> ret = new ArrayList<Double>();
     	Queue<TreeNode> q = new LinkedList<TreeNode>();
