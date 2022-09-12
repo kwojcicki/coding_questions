d63b3ad427e66605d4a4fb256d2c473a5fb7b9e1
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-09-11 20:00:17 -0700

    bag of tokens

--- leetcode/src/solutions/bag_of_tokens.java
+++ leetcode/src/solutions/bag_of_tokens.java
@@ -3,6 +3,29 @@ package solutions;
 import java.util.Arrays;
 
 public class bag_of_tokens {
+
+    public int bagOfTokensScore1(int[] tokens, int power) {
+    	Arrays.sort(tokens);
+        int ret = 0, curr = 0;
+        int start = 0, end = tokens.length - 1;
+        while(start <= end) {
+        	if(power >= tokens[start]) {
+        		power -= tokens[start];
+        		start++;
+        		curr++;
+            } else if(curr == 0){ 
+                break;
+        	} else {
+        		power += tokens[end];
+        		end--;
+        		curr--;
+        	}
+        	ret = Math.max(ret, curr);
+        }
+        
+        return ret;
+    }
+	
 	public int bagOfTokensScore(int[] tokens, int P) {
 		int ret = 0;
 		Arrays.sort(tokens);
