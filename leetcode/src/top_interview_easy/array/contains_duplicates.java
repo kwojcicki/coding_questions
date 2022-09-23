c3e2d50c629b05ebf022dc2b41e175850c96ef56
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-09-22 17:24:04 -0700

    contains duplicates

--- leetcode/src/top_interview_easy/array/contains_duplicates.java
+++ leetcode/src/top_interview_easy/array/contains_duplicates.java
@@ -1,14 +1,25 @@
 package top_interview_easy.array;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class contains_duplicates {
-    public boolean containsDuplicate(int[] nums) {
-        for(int i = 0; i < nums.length; i++) {
-        	for(int j = i + 1; j < nums.length; j++) {
-        		if(nums[i] == nums[j]) {
-        			return true;
-        		}
-        	}
-        }
-        return false;
-    }
+	public boolean containsDuplicate(int[] nums) {
+		Set<Integer> seen = new HashSet<>();
+		for(int i: nums) {
+			if(!seen.add(i)) return true;
+		}
+		return false;
+	}
+
+	public boolean containsDuplicate1(int[] nums) {
+		for(int i = 0; i < nums.length; i++) {
+			for(int j = i + 1; j < nums.length; j++) {
+				if(nums[i] == nums[j]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
 }
