074754be677a8abf818ef19fd19fbbc8d342dcd7
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-09-30 21:50:55 -0700

    decode ways

--- leetcode/src/solutions/decode_ways.java
+++ leetcode/src/solutions/decode_ways.java
@@ -3,7 +3,27 @@ package solutions;
 import java.util.Arrays;
 
 public class decode_ways {
+	
 	public int numDecodings(String s) {
+		int dp1 = 1, dp2 = 0;
+		int n = s.length();
+		char[] c = s.toCharArray();
+		for(int i = n - 1; i >= 0; i--) {
+			int sum = c[i] == '0' ? 0 : dp1;
+			if(i + 1 <= c.length - 1 && (
+	    			c[i] == '1' || (c[i] == '2' &&  c[i + 1] < '7') 
+	    			)) {
+				sum += dp2;
+			}
+			
+			dp2 = dp1;
+			dp1 = sum;
+		}
+		
+		return dp1;
+    }
+	
+	public int numDecodings1(String s) {
 		int[] dp = new int[s.length() + 1];
 		Arrays.fill(dp, 0);
         dp[dp.length - 1] = 1;
