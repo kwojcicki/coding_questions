f1c8120625445d8b65b89f4d302c18ff02818d32
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-10-27 14:58:47 -0700

    image overlap

--- leetcode/src/solutions/image_overlap.java
+++ leetcode/src/solutions/image_overlap.java
@@ -4,10 +4,9 @@ public class image_overlap {
     public int largestOverlap(int[][] A, int[][] B) {
     	int max = 0;
     	
-    	for(int r = 0; r < A.length; r++) {
-    		for(int c = 0; c < A[r].length; c++) {
+    	for(int r = -A.length + 1; r < A.length - 1; r++) {
+    		for(int c = -A[0].length + 1; c < A[0].length - 1; c++) {
     			max = Math.max(max, shift(A, B, r, c));
-    			max = Math.max(max, shift(B, A, r, c));
     		}
     	}
     	
@@ -17,16 +16,12 @@ public class image_overlap {
     public int shift(int[][] A, int[][] B, int rs, int cs) {
     	int count = 0;
     	
-    	int bRow = 0;
-    	for(int r = rs; r < A.length; r++) {
-    		int bCol = 0;
-    		for(int c = cs; c < A[r].length; c++) {
-    			if(A[r][c] == 1 && B[bRow][bCol] == A[r][c]) {
-    				count++;
-    			}
-    			bCol++;
+    	for(int r = 0; r < A.length; r++) {
+    		if(r + rs < 0 || r + rs >= A.length) continue;
+    		for(int c = 0; c < A[r].length; c++) {
+    			if(c + cs < 0 || c + cs >= A[0].length) continue;
+    			if(A[r][c] == 1 && B[r + rs][c + cs] == 1) count++;
     		}
-    		bRow++;
     	}
     	
     	return count;
