0527918e9dac3a4b5584b61e8440873bf1c848d8
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-10-30 17:22:43 -0700

    toeplitz matrix

--- leetcode/src/solutions/toeplitz_matrix.java
+++ leetcode/src/solutions/toeplitz_matrix.java
@@ -2,6 +2,18 @@ package solutions;
 
 public class toeplitz_matrix {
 	
+	public boolean isToeplitzMatrix3(int[][] matrix) {
+		for(int i = 1; i < matrix.length; i++){
+            for(int j = 1; j < matrix[0].length; j++){
+                if(matrix[i][j] != matrix[i - 1][j - 1]){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+	}
+	
 	public boolean isToeplitzMatrix2(int[][] matrix) {
 		for(int i = 0; i < matrix.length - 1; i++) {
 			for(int j = 0; j < matrix[i].length - 1; j++) {
