96f172f3391f1d775aca6cb9b0b0637c6bef638b
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-11-26 16:52:12 -0800

    arithmetic slices

--- leetcode/src/arithmetic_slices.java
+++ leetcode/src/arithmetic_slices.java
@@ -1,6 +1,37 @@
 
 public class arithmetic_slices {
-    public int numberOfArithmeticSlices(int[] nums) {
+	public int numberOfArithmeticSlices(int[] nums) {
+    	if(nums == null || nums.length < 3) return 0;
+    	int curr = 0;
+    	int ret = 0;
+    	
+    	for(int i = 2; i < nums.length; i++) {
+    		if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+    			curr += 1;
+    			ret += curr;
+    		} else {
+    			curr = 0;
+    		}
+    	}
+    	
+    	return ret;
+    }
+	
+    public int numberOfArithmeticSlices1(int[] nums) {
+    	if(nums == null || nums.length < 3) return 0;
+    	int[] dp = new int[nums.length];
+    	int ret = 0;
+    	for(int i = 2; i < dp.length; i++) {
+    		if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+    			dp[i] = dp[i - 1] + 1;
+    		}
+    		ret += dp[i];
+    	}
+    	
+    	return ret;
+    }
+    
+    public int numberOfArithmeticSlices2(int[] nums) {
     	int ret = 0;
     	
     	if(nums == null || nums.length < 3) {
