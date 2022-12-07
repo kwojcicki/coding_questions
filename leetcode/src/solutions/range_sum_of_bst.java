0da04f699a32ba9e0961c3dd5bf0f9f82d90d303
Krystian Wojcicki <kwojcicki@sympatico.ca>
2022-12-06 17:00:46 -0800

    range sum of bst

--- leetcode/src/solutions/range_sum_of_bst.java
+++ leetcode/src/solutions/range_sum_of_bst.java
@@ -2,11 +2,9 @@ package solutions;
 
 public class range_sum_of_bst {
     public int rangeSumBST(TreeNode root, int low, int high) {
-        if(root == null) {
-        	return 0;
-        }
-        
-        return (root.val >= low && root.val <= high ? root.val : 0) + 
+        return root == null ? 
+        	0 : 
+        	(root.val >= low && root.val <= high ? root.val : 0) + 
         		rangeSumBST(root.left, low, high) + 
         		rangeSumBST(root.right, low, high);
     }
