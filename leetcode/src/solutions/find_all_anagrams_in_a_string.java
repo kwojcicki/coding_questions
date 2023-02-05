4ad845ee8a7c7026a3b4d8865bb087692b41329c
Krystian Wojcicki <kwojcicki@sympatico.ca>
2023-02-04 16:22:14 -0800

    find all anagrams in a string

--- leetcode/src/solutions/find_all_anagrams_in_a_string.java
+++ leetcode/src/solutions/find_all_anagrams_in_a_string.java
@@ -1,36 +1,33 @@
 package solutions;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class find_all_anagrams_in_a_string {
-	public List<Integer> findAnagrams(String s, String p) {
+	public List<Integer> findAnagrams(String s2, String s1) {
 		List<Integer> ret = new ArrayList<>();
 
-		char[] curr = new char[26];
-		char[] past = new char[26];
+        char[] c = new char[26];
+        for(char c1: s1.toCharArray()) c[c1 - 'a']++;
+        int req = s1.length();
 
-		for(char c: p.toCharArray()) {
-			past[c - 'a']++;
-		}
+        int start = 0;
+        for(int end = 0; end < s2.length(); end++){
+            while(c[s2.charAt(end) - 'a'] <= 0 && start < end){
+                req++;
+                c[s2.charAt(start++) - 'a']++;
+            }
 
-		for(int i = 0; i < s.length() && i < p.length(); i++) {
-			curr[s.charAt(i) - 'a']++;
-		}
+            if(c[s2.charAt(end) - 'a'] > 0){
+                c[s2.charAt(end) - 'a']--;
+                req--;
+                if(req == 0) ret.add(start);
+            } else {
+                start = end + 1;
+            }
+        }
 
-		for(int i = p.length(); i < s.length(); i++) {
-			if(Arrays.equals(curr, past)) {
-				ret.add(i - p.length());
-			}
-
-			curr[s.charAt(i - p.length()) - 'a']--;
-			curr[s.charAt(i) - 'a']++;
-		}
-		if(Arrays.equals(curr, past)) {
-			ret.add(s.length() - p.length());
-		}
-		return ret;
+        return ret;
 	}
 	
 	public List<Integer> findAnagrams1(String s, String p) {
