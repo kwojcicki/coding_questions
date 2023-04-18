c8401c38919fe782e36faf6b62916f0050064abb
Krystian Wojcicki <kwojcicki@sympatico.ca>
2023-04-17 17:36:37 -0700

    merge strings alternatively

---
+++ leetcode/src/solutions/merge_strings_alternately.java
@@ -0,0 +1,15 @@
+package solutions;
+
+public class merge_strings_alternately {
+	public String mergeAlternately(String word1, String word2) {
+		StringBuilder sb = new StringBuilder(word1.length() + word2.length());
+		int i = 0, j = 0;
+		while(i < word1.length() && j < word2.length()) {
+			sb.append(word1.charAt(i++));
+			sb.append(word2.charAt(j++));
+		}
+		while(i < word1.length()) sb.append(word1.charAt(i++));
+		while(j < word2.length()) sb.append(word2.charAt(j++));
+		return sb.toString();
+	}
+}
