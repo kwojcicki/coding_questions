0080da80ce8fb9fbf4702dc72192de29d62096ee
Krystian Wojcicki <kwojcicki@sympatico.ca>
2023-05-06 20:16:20 -0700

    find the longest valid obstacle course at each position

---
+++ leetcode/src/solutions/find_the_longest_valid_obstacle_course_at_each_position.java
@@ -0,0 +1,27 @@
+package solutions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class find_the_longest_valid_obstacle_course_at_each_position {
+    public int[] longestObstacleCourseAtEachPosition(int[] obstacles) {
+    	List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < obstacles.length; i++) {
+        	int x = obstacles[i];
+        	if(list.isEmpty() || list.get(list.size() - 1) <= x) {
+        		list.add(x);
+        		obstacles[i] = list.size();
+        	} else {
+        		int idx = Collections.binarySearch(list, x);
+        		if(idx < 0) idx = -(idx) - 1;
+        		while(list.get(idx) == x) idx++;
+						// System.out.println(list + " " + x + " " + idx);
+        		list.set(idx, x);
+        		obstacles[i] = idx + 1;
+        	}
+        }
+    	return obstacles;
+    }
+}
