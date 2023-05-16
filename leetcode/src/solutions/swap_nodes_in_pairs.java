37d511f79b33a9e6361190fced4ddf0264e7ba80
Krystian Wojcicki <kwojcicki@sympatico.ca>
2023-05-15 21:22:31 -0700

    swap nodes in pairs recursive

--- leetcode/src/solutions/swap_nodes_in_pairs.java
+++ leetcode/src/solutions/swap_nodes_in_pairs.java
@@ -1,6 +1,20 @@
 package solutions;
 
 public class swap_nodes_in_pairs {
+	
+    public ListNode swapPairsRecursive(ListNode head) {
+        if(head == null) return null;
+        if(head.next == null) return head;
+
+        ListNode ret = head.next;
+        head.next = head.next.next;
+        ret.next = head;
+
+        head.next = swapPairsRecursive(head.next);
+        
+        return ret;
+    }
+    
     public ListNode swapPairs(ListNode head) {
         if(head == null) {
     		return null;
