4e3171fc1880f840ad5237c4c9b27f42a8748470
kwojcicki <kwojcicki@sympatico.ca>
2024-01-03 19:59:39 -0500

    minimum number of operations to make array empty

--- leetcode/src/solutions/minimum_number_of_operations_to_make_array_empty.java
+++ leetcode/src/solutions/minimum_number_of_operations_to_make_array_empty.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public class minimum_number_of_operations_to_make_array_empty {
     public int minOperations(int[] nums) {
-        int ret = 0;
+    	int ret = 0;
         Map<Integer, Integer> cnts = new HashMap<>();
         
         for(int i: nums) {
@@ -14,14 +14,10 @@ public class minimum_number_of_operations_to_make_array_empty {
         
         for(int i: cnts.values()) {
         	if(i == 1) return -1;
-        	else if(i == 2) ret += 1;
-        	else if(i == 3) ret += 1;
-        	else {
             	int leftover = i % 3;
             	if(leftover == 0) ret += i / 3;
             	else if(leftover == 1) ret += (i - 3) / 3 + 2;
             	else if(leftover == 2) ret += i / 3 + 1;
-        	}
         	
         }
         
