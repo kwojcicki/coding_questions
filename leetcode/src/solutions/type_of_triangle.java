6bdcfbd894299da4eedd1c26b4ff6795fdcafb71
Krystian Wojcicki <kwojcicki@sympatico.ca>
2025-05-19 16:04:19 -0400

    type of triangle

---
+++ leetcode/src/solutions/type_of_triangle.java
@@ -0,0 +1,23 @@
+package solutions;
+
+public class type_of_triangle {
+    public String triangleType(int[] nums) {
+        if(
+            nums[0] + nums[1] <= nums[2] ||
+            nums[1] + nums[2] <= nums[0] || 
+            nums[0] + nums[2] <= nums[1]
+        ){
+            return "none";
+        } else if(nums[0] == nums[1] && nums[1] == nums[2]){
+            return "equilateral";
+        } else if(
+            nums[0] == nums[1] || 
+            nums[1] == nums[2] ||
+            nums[0] == nums[2]
+        ){
+            return "isosceles";
+        }
+
+        return "scalene";
+    }
+}
