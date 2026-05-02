ad7dbc4844923fc2d5dbd79ec290125c7f64ca5e
Krystian <kwojcicki@sympatico.ca>
2026-05-02 19:22:39 -0400

    Implement rotatedDigits1 method for rotated digits
    Added a new method 'rotatedDigits1' to calculate rotated digits using dynamic programming.

--- leetcode/src/solutions/rotated_digits.java
+++ leetcode/src/solutions/rotated_digits.java
@@ -19,4 +19,30 @@ public class rotated_digits {
         	return false;
         }).count();
     }
+
+    
+    public int rotatedDigits1(int n) {
+        if(n < 2) return 0;
+        else if(n < 5) return 1;
+        else if(n < 6) return 2;
+        else if(n < 9) return 3;
+       int[] dp = new int[n + 1];
+       int ret = 4;
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[5] = 2;
+        dp[6] = 2;
+        dp[8] = 1;
+        dp[9] = 2;
+        for(int i = 10; i <= n; i++){
+            int left = dp[i / 10]; int right = dp[i % 10];
+            if(left == 1 && right == 1) dp[i] = 1;
+            else if(left >= 1 && right >= 1) { ret++; dp[i] = 2; }
+            else dp[i] = 0;
+        }
+
+       return ret;
+    }
 }
